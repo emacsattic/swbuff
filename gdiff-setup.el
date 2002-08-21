925363777562c44b34ca05859c6f57b778e54906
ponce <ponce>
2002-08-21 13:31:20 +0000

    Initial revision.

---
+++ gdiff-setup.el
@@ -0,0 +1,155 @@
+;; gdiff-setup.el -- gdiff setup
+
+;; Copyright (C) 2002 by David Ponce
+
+;; Author: David Ponce <david@dponce.com>
+;; Maintainer: David Ponce <david@dponce.com>
+;; Created: March 15 2000
+;; Keywords: tools
+;; X-RCS: $Id: gdiff-setup.el,v 1.1 2002/08/21 13:31:20 ponce Exp $
+
+;; This file is not part of Emacs
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; This is the always-loaded portion of gdiff.  It setup gdiff
+;; shortcut keys, menu and autoloads
+
+;; Support
+;;
+;; This program is available at <http://www.dponce.com/>. Any
+;; comments, suggestions, bug reports or upgrade requests are welcome.
+;; Please send them to David Ponce at <david@dponce.com>
+
+;;; History::
+
+;;; Code:
+(require 'vc)
+
+;;; Compatibility
+(or (fboundp 'vc-ensure-vc-buffer)
+    
+    ;; Copied from GNU Emacs vc.el
+    (defun vc-ensure-vc-buffer ()
+      "Make sure that the current buffer visits a version-controlled file."
+      (if vc-dired-mode
+          (set-buffer (find-file-noselect (dired-get-filename)))
+        (while vc-parent-buffer
+          (pop-to-buffer vc-parent-buffer))
+        (if (not (buffer-file-name))
+            (error "Buffer %s is not associated with a file" (buffer-name))
+          (if (not (vc-backend (buffer-file-name)))
+              (error "File %s is not under version control" (buffer-file-name))))))
+    
+    )
+
+(defgroup gdiff nil
+  "Use a GUI diff tool from Emacs."
+  :group 'tools)
+
+(defcustom gdiff-program "winmerge.exe"
+  "*Path to the external GUI diff tool."
+  :group 'gdiff
+;;  :type '(file :must-match))
+  :type 'file)
+
+(defcustom gdiff-load-hook nil
+  "*Hook run when package has been loaded."
+  :group 'gdiff
+  :type 'hook)
+
+(defvar gdiff-menu
+  (list "Compare (GUI)"
+        ["Buffer Changes"
+         gdiff-buffer-with-file
+         (buffer-file-name)]
+        ["Two Files..."
+         gdiff-files
+         t]
+        ["Two Folders..."
+         gdiff-folders
+         t]
+        ["File with Revision..."
+         gdiff-revision
+         (gdiff-check-if-vc-buffer)]
+        ["With Last Version"
+         gdiff-latest-revision
+         (gdiff-check-if-vc-buffer)]
+        )
+  "The gdiff menu.")
+
+(defvar gdiff-prefix-map nil
+  "The gdiff prefix keymap.")
+
+;;;### (autoloads (gdiff-revision gdiff-latest-revision gdiff-folders
+;;;;;;  gdiff-files gdiff-buffer-with-file) "gdiff" "gdiff.el" (15715
+;;;;;;  37770))
+;;; Generated autoloads from gdiff.el
+
+(autoload (quote gdiff-buffer-with-file) "gdiff" "\
+View the differences between BUFFER and its associated file." t nil)
+
+(autoload (quote gdiff-files) "gdiff" "\
+Compare FILE1 with FILE2." t nil)
+
+(autoload (quote gdiff-folders) "gdiff" "\
+Compare FOLDER1 with FOLDER2." t nil)
+
+(autoload (quote gdiff-latest-revision) "gdiff" "\
+Compare FILE with its most recent checked in version.
+Optional argument FILE default to the file visited by the current
+buffer." t nil)
+
+(autoload (quote gdiff-revision) "gdiff" "\
+Compare two revisions of FILE.
+Optional argument FILE default to the file visited by the current
+buffer." t nil)
+
+;;;***
+
+(defun gdiff-check-if-vc-buffer ()
+  "Return t if the current buffer visits a version-controlled file."
+  (condition-case nil
+      (progn
+        (vc-ensure-vc-buffer)
+        t)
+    (error nil)))
+
+;; Add the gdiff menu before the Tools/Compare menu
+(if (and (condition-case nil
+             (require 'easymenu "easymenu")
+           (error nil))
+         (fboundp 'easy-menu-add-item))
+    (easy-menu-add-item nil '("tools") gdiff-menu "compare"))
+
+;; Setup gdiff keys
+(setq gdiff-prefix-map (lookup-key global-map "\C-c="))
+
+(if (not (keymapp gdiff-prefix-map))
+    (progn
+      (setq gdiff-prefix-map (make-sparse-keymap))
+      (define-key global-map "\C-c=" gdiff-prefix-map)
+      (define-key gdiff-prefix-map "b" 'gdiff-buffer-with-file)
+      (define-key gdiff-prefix-map "f" 'gdiff-files)
+      (define-key gdiff-prefix-map "d" 'gdiff-folders)
+      (define-key gdiff-prefix-map "r" 'gdiff-revision)
+      (define-key gdiff-prefix-map "=" 'gdiff-latest-revision)))
+
+(provide 'gdiff-setup)
+
+;;; gdiff-setup.el ends here
