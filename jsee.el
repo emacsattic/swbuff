4763f55befb03df2293e1cd53dec059b224139c4
ponced <ponced>
2003-04-25 11:29:56 +0000

    Updated comments. (jsee-version): Moved. Updated to 2.1. (jde-jsee-load-hook, jsee-call, jsee-delete-file) (jsee-get-package-name, jsee-file-class) (jde-jsee-javadoc, jde-jsee-javadoc-output-dir) (jsee-javadoc-get-output-dir) (jsee-javadoc-get-classpath-option) (jsee-javadoc-get-output-filename) (jsee-javadoc-delete-previous-output) (jsee-javadoc-browse-output, jsee-pre-generate-handler) (jsee-post-generate-handler, jsee-make-doc-generator-command) (jsee-run-doc-generator, jsee-browse-api-doc) (jsee-default-load-hook): Doc fix & some code cleanup. (jsee-customize): Removed.

--- jsee.el
+++ jsee.el
@@ -1,13 +1,14 @@
-;; jsee.el --- Java source documentation viewer
+;;; jsee.el --- Java source documentation viewer
 
-;; Copyright (C) 1998, 2000 by David Ponce
+;; Copyright (C) 1998, 2000, 2003 by David Ponce
 
 ;; Author: David Ponce <david@dponce.com>
 ;; Maintainer: David Ponce <david@dponce.com>
 ;; Created: 3 Dec 1998
-;; Version: 2.0
 ;; Keywords: jde
-;; VC: $Id: jsee.el,v 1.9 2001/10/29 15:49:01 ponce Exp $
+;; Revision: $Id: jsee.el,v 1.10 2003/04/25 11:29:56 ponced Exp $
+
+(defconst jsee-version "2.1")
 
 ;; This file is not part of Emacs
 
@@ -27,93 +28,29 @@
 ;; Boston, MA 02111-1307, USA.
 
 ;;; Commentary:
-
+;;
 ;; This package is a JDE add-on which automatically generates and view
 ;; documentation generated from the source of the java file in the
 ;; current buffer.  The default implementation uses javadoc as
 ;; generator and view the documentation with the default browser.
-
-;; Installation:
 ;;
-;; Put this file on your Emacs load-path and add (require 'jsee) into
-;; your Emacs startup file.
-
-;; Usage:
-;;
-;; M-x `jsee-browse-api-doc' to generates and view the source
+;; M-x `jsee-browse-api-doc' generate and view the source
 ;; documentation of the java file in the current buffer.
 ;;
-;; M-x `jsee-customize' to customize all the jsee options.
+;; To install and use, put this file on your Emacs-Lisp load path and
+;; add the following into your ~/.emacs startup file:
+;;
+;; (require 'jsee)
+;;
 
-;; Support
-;;
-;; This program is available at <http://www.dponce.com/>. Any
-;; comments, suggestions, bug reports or upgrade requests are welcome.
-;; Please send them to David Ponce at <david@dponce.com>
-
-;;; Change Log:
-
-;; $Log: jsee.el,v $
-;; Revision 1.9  2001/10/29 15:49:01  ponce
-;; (jsee-lookup-java1-javadoc): New function.  From JDE 2.2.5.2
-;; `jde-help-lookup-java1-javadoc' which is no more available in recent
-;; versions of the JDE.
-;; (jsee-lookup-java2-javadoc): New function.  From JDE 2.2.5.2
-;; `jde-help-lookup-java2-javadoc' which is no more available in recent
-;; versions of the JDE.
-;; (see-javadoc-get-output-filename): Use them.
-;; (jsee-javadoc-delete-previous-output): Ditto.
-;;
-;; Revision 1.8  2000/09/29 09:29:49  david_ponce
-;; Updated to new semantic API.
-;;
-;; Revision 1.7  2000/09/04 13:58:20  david_ponce
-;; (jsee-browse-api-doc): Fixed XEmacs `local-variable-p' compatibility
-;; problem.
-;;
-;; Thanks to Aaron Brashears <aaron@myplay.com> who has reported this
-;; bug.
-;;
-;; Revision 1.6  2000/08/11 14:59:51  david_ponce
-;; Major rewrite:
-;;
-;; Follow standard Emacs coding convention.
-;; Better separation between generic and javadoc specific code.
-;; Use JDE semantic bovinator parser to retrieve the package statement.
-;; Automatically handle javadoc version 1 or version 2 HTML style.
-;; Some customizable variable names have changed so update you
-;; configuration.
-;;
-;; Revision 1.5  1999/07/05 21:27:11  ebat311
-;; Use Emacs/XEmacs compatible key mapping in `jsee-default-load-hook'.
-;;
-;; Revision 1.4  1999-05-26 10:22:30+02  ebat311
-;; Fixed - XEmacs compatibility of key mapping in
-;; `jsee-default-load-hook'.
-;;
-;; Revision 1.3  1999-04-22 23:28:44+02  ebat311
-;; Added `autoload' cookies.
-;;
-;; Revision 1.2  1999-03-01 14:56:25+01  ebat311
-;; FIXED - when a java file doesn't have a package name the
-;; `jsee-get-javadoc-url' and `jsee-get-javadoc1.2-url' functions
-;; built invalid URL.
-;; Thanks to Leif Jonsson <albedo@hem2.passagen.se> who
-;; has reported this bug and suggested the correction.
-;;
-;; Revision 1.1  1998/12/08 09:43:55  ebat311
-;; Initial revision
+;;; History:
 ;;
 
 ;;; Code:
 (require 'jde)
-
-(defconst jsee-version "2.0 $Date: 2001/10/29 15:49:01 $"
-  "jsee version information.")
-
-;;;
-;;; Global customization
-;;;
+
+;;; Options
+;;
 
 ;; ALL CUSTOMIZABLE VARIABLES ARE PREFIXED WITH `jde-' TO ALLOW THEIR
 ;; SAVING IN A JDE PROJECT FILE.
@@ -155,34 +92,33 @@ javadoc for the current Java file."
   :type 'function)
 
 (defcustom jde-jsee-load-hook '(jsee-default-load-hook)
-  "*Hook run when package has been loaded.
+  "Hook run when package has been loaded.
 The default hook provided `jsee-default-load-hook' maps the `jde-mode'
 key `C-f1' to the `jsee-browse-api-doc' command."
   :group 'jde-jsee
   :type 'hook)
-
-;;;
+
 ;;; Common functions
-;;;
-
+;;
 (defun jsee-fullpath (path)
   "Return the full path of the given PATH.
 ~ (HOME) and environment variables references are expanded."
   (expand-file-name (substitute-in-file-name path)))
 
 (defun jsee-call (fvar &optional with-error &rest with-args)
-  "Wrapper function to `funcall'. FVAR is a symbol bound to the
-function to be called. If optional parameter WITH-ERROR in non-nil an
-error is signaled if FVAR does not define a valid function. WITH-ARGS
-defines the parameters passed to the function."
+  "Wrapper to the function `funcall'.
+FVAR is a symbol bound to the function to be called.
+If optional argument WITH-ERROR is non-nil, signal an error if FVAR is
+not a valid function.
+WITH-ARGS are the parameters passed to the function."
   (if (and (symbolp fvar) (fboundp fvar))
       (apply fvar with-args)
     (if with-error
         (error "Invalid function %s" fvar))))
 
 (defun jsee-delete-file (file)
-  "Helper function used to delete the file FILE. Returns non-nil if
-the file is deleted or nil if not."
+  "Helper function used to delete the file FILE.
+Return non-nil if the file is deleted or nil if not."
   (if (stringp file)
       (condition-case err
           (progn
@@ -195,40 +131,33 @@ the file is deleted or nil if not."
 
 (defun jsee-get-package-name ()
   "Return the package name in the current file.
-This function uses the semantic bovinator parser table to find the
-package statement."
-  (let* ((tokens (semantic-bovinate-toplevel t))
-         (packages (semantic-find-nonterminal-by-token 'package tokens)))
+This function uses semantic to find the package statement."
+  (let ((packages (semantic-find-nonterminal-by-token
+                   'package (current-buffer))))
     (if packages
         (semantic-token-name (car packages)))))
 
 (defun jsee-get-file-class ()
   "Return the qualified class name of the current file."
   (let ((package (jsee-get-package-name)))
-    (setq package
-          (if package
-              (concat package ".")
-            ""))
-    (concat package
+    (concat (if package (concat package ".") "")
             (file-name-sans-extension
              (file-name-nondirectory buffer-file-name)))))
 
 (defvar jsee-file-class nil
-  "Hold the qualified class name of the current file. This variable is
-local to the compilation buffer and can be used by the
-`jde-jsee-post-generate-function'.")
-
-;;;
-;;; Specific to javadoc generator
-;;;
-
+  "Hold the qualified class name of the current file.
+This variable is local to the compilation buffer and can be used by
+the `jde-jsee-post-generate-function'.")
+
+;;; Options specific to the javadoc generator
+;;
 (defgroup jde-jsee-javadoc nil
-  "jsee customization specific to the javadoc generator."
+  "Options specific to the jsee javadoc generator."
   :group 'jde-jsee
   :prefix "jde-jsee-javadoc-")
 
 (defcustom jde-jsee-javadoc-output-dir "$TEMP/jsee"
-  "*The working directory in which javadoc will generate HTML files.
+  "*The working directory where javadoc will generate HTML files.
 This directory must exists. For better results with JDK 1 javadoc
 style you can copy into it the 'images' sub-directory of the JDK 1 API
 documentation."
@@ -288,17 +217,22 @@ jsee."
   "*If non-nil javadoc will show all classes and members."
   :group 'jde-jsee-javadoc
   :type 'boolean)
-
+
+;;; Javadoc specific
+;;
 (defun jsee-javadoc-get-output-dir ()
-  "Return the full directory name from `jde-jsee-javadoc-output-dir'
-or `default-directory' if nil."
+  "Return the directory where javadoc will generate HTML files.
+That is, `jde-jsee-javadoc-output-dir' if non-nil, else
+`default-directory'."
   (if (stringp jde-jsee-javadoc-output-dir)
-      (file-name-as-directory (jsee-fullpath jde-jsee-javadoc-output-dir))
+      (file-name-as-directory
+       (jsee-fullpath jde-jsee-javadoc-output-dir))
     default-directory))
 
 (defun jsee-javadoc-get-classpath-option ()
-  "Return the javadoc -classpath value from respectively
-`jde-compile-option-classpath' or `jde-global-classpath'."
+  "Return the javadoc -classpath value.
+From respectively `jde-compile-option-classpath' or
+`jde-global-classpath'."
   (if jde-compile-option-classpath
       (jde-build-classpath-arg jde-compile-option-classpath
                                jde-quote-classpath)
@@ -352,24 +286,22 @@ or `default-directory' if nil."
 	doc-path)))
 
 (defun jsee-javadoc-get-output-filename ()
-  "Return the javadoc (version 1 or 2) HTML file name associated to
-the current Java file. If not found return nil."
+  "Return the name of the javadoc HTML file, from this Java file.
+If not found return nil."
   (let ((docset (jsee-javadoc-get-output-dir))
         (class  jsee-file-class))
     (or (jsee-lookup-java1-javadoc class docset)
         (jsee-lookup-java2-javadoc class docset))))
 
 (defun jsee-javadoc-delete-previous-output ()
-  "Remove any previous javadoc (all versions) HTML file generated for
-the current Java file."
+  "Remove any previous javadoc HTML file."
   (let ((docset (jsee-javadoc-get-output-dir))
         (class  jsee-file-class))
     (jsee-delete-file (jsee-lookup-java1-javadoc class docset))
     (jsee-delete-file (jsee-lookup-java2-javadoc class docset))))
   
 (defun jsee-javadoc-browse-output ()
-  "Browse the HTML file generated by javadoc (version 1 or 2) for the
-current Java file."
+  "Browse the generated javadoc file."
   (let ((output (jsee-javadoc-get-output-filename)))
     (cond (output
            (message "browse-url-of-file %s" output)
@@ -378,13 +310,11 @@ current Java file."
            (message "Doc file not found for %s in %s"
                     jsee-file-class
                     (jsee-javadoc-get-output-dir))))))
-
-;;;
+
 ;;; Doc generator process handling
-;;;
-
+;;
 (defun jsee-pre-generate-handler ()
-  "Handle operations done before running the doc generator."
+  "Handle operations run before the doc generator."
   (message "jsee-pre-generate-handler in buffer %s" (buffer-name))
   (make-local-variable 'jsee-file-class)
   (jsee-call jde-jsee-pre-generate-function)
@@ -392,68 +322,44 @@ current Java file."
        'jsee-post-generate-handler))
 
 (defun jsee-post-generate-handler (buffer msg)
-  "Handle operation done after running the doc generator."
+  "Handle operations run after the doc generator."
   (message "jsee-post-generate-handler in buffer %s" (buffer-name))
   (if (string-equal msg "finished\n")
       (jsee-call jde-jsee-post-generate-function)))
 
 (defun jsee-make-doc-generator-command ()
-  "Return the full command used to generate the Java documentation."
+  "Return the command to generate the Java documentation."
   (concat jde-jsee-doc-generator " " 
-          (jsee-call jde-jsee-get-doc-generator-options-function 'with-error) 
+          (jsee-call jde-jsee-get-doc-generator-options-function 'with-error)
           " "
           (file-name-nondirectory buffer-file-name)))
 
 (defun jsee-run-doc-generator ()
   "Run the Java API Documentation Generator."
-  (let ((compile-command (jsee-make-doc-generator-command)))
-    ;; Force save-some-buffers to use the minibuffer
-    ;; to query user about whether to save modified buffers.
-    ;; Otherwise, when user invokes jde-compile from
-    ;; menu, save-some-buffers tries to popup a menu
-    ;; which seems not to be supported--at least on
-    ;; the PC.
-    (if (eq system-type 'windows-nt)
-        (let ((temp last-nonmenu-event))
-          ;; The next line makes emacs think that jde-compile
-          ;; was invoked from the minibuffer, even when it
-          ;; is actually invoked from the menu-bar.
-          (setq last-nonmenu-event t)
-          (save-some-buffers (not compilation-ask-about-save) nil)
-          (setq last-nonmenu-event temp))
-      (save-some-buffers (not compilation-ask-about-save) nil))
-    (let ((compilation-process-setup-function 'jsee-pre-generate-handler))
-      (setq jsee-file-class (jsee-get-file-class))
-      (compile-internal compile-command "No more errors"))))
-
-;;;
-;;; Hooks and commands
-;;;
+  (let ((compile-command (jsee-make-doc-generator-command))
+        (compilation-process-setup-function 'jsee-pre-generate-handler))
+    (save-some-buffers (not compilation-ask-about-save) nil)
+    (setq jsee-file-class (jsee-get-file-class))
+    (compile-internal compile-command "No more errors")))
+
+;;; Command & hook
+;;
 
 ;;;###autoload
 (defun jsee-browse-api-doc ()
-  "Run the Java API Documentation Generator for the Java source file
-in the current buffer and browse the resulting HTML file."
+  "Browse the Java API Documentation of the current Java file."
   (interactive)
-  (if (eq major-mode 'jde-mode)
-      (if (local-variable-p 'semantic-toplevel-bovine-table
-          (current-buffer))             ; XEmacs compatibility
-    (jsee-run-doc-generator)
-        (error "This command needs a version of JDE with the semantic bovinator."))
-    (error "This command must be run in 'jde-mode'.")))
-
-;;;###autoload
-(defun jsee-customize ()
-  "Customization of the group jde-jsee."
-  (interactive)
-  (customize-group "jde-jsee"))
+  (if (and (eq major-mode 'jde-mode) (semantic-active-p))
+      (jsee-run-doc-generator)
+    (error "Not in `jde-mode', or buffer not set up for parsing.")))
 
 (defun jsee-default-load-hook ()
-  "Default hook run when package has been loaded. It maps C-f1 to
-`jsee-browse-api-doc'."
+  "Default hook run when package has been loaded.
+Define the keyboard shortcut [C-f1] for `jsee-browse-api-doc'."
   (define-key java-mode-map [(control f1)] 'jsee-browse-api-doc))
 
 (provide 'jsee)
+
 (run-hooks 'jde-jsee-load-hook)
 
 ;;; jsee.el ends here
